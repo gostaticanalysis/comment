25be1d036f505dfcf34c056a4719e831c219a438
Nick Miyake <nmiyake@gmail.com>
2020-08-24 23:29:33 -0700

    Fix comment directive parsing in Go 1.15+
    Go 1.15 changed the behavior of *ast.CommentGroup.Text() in a way
    that breaks the functionality of this package. Updates the implementation
    to support the new behavior.
    
    Fixes #7

--- comment_test.go
+++ comment_test.go
@@ -16,7 +16,7 @@ func TestMaps_CommentsByPosLine(t *testing.T) {
 		want []string
 	}{
 		"single": {"testdata/Maps_CommentsByPosLine/single.go", []string{"a"}},
-		"multi": {"testdata/Maps_CommentsByPosLine/multi.go", []string{"b"}},
+		"multi":  {"testdata/Maps_CommentsByPosLine/multi.go", []string{"b"}},
 	}
 
 	for n, tt := range cases {
@@ -34,6 +34,34 @@ func TestMaps_CommentsByPosLine(t *testing.T) {
 
 			if diff := cmp.Diff(tt.want, got); diff != "" {
 				t.Errorf(diff)
+			}
+		})
+	}
+}
+
+func TestMaps_IgnoreLine(t *testing.T) {
+	t.Parallel()
+
+	cases := map[string]struct {
+		path  string
+		check string
+		line  int
+		want  bool
+	}{
+		"has_directive": {"testdata/Maps_IgnorePos/has_directive.go", "test-check", 4, true},
+		"no_directive":  {"testdata/Maps_IgnorePos/no_directive.go", "test-check", 4, false},
+	}
+
+	for n, tt := range cases {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+			fset := token.NewFileSet()
+			ms := maps(t, fset, tt.path)
+
+			got := ms.IgnoreLine(fset, tt.line, tt.check)
+			if tt.want != got {
+				t.Errorf("want %v, got %v", tt.want, got)
 			}
 		})
 	}
